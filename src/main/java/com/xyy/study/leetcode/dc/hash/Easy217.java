11a56cfdfde61ee9c6590e81606fd615266b32cd
xiayuyang <xiayuyang@stargraph.cn>
2023-05-08 08:59:09 +0800

    哈希表

---
+++ src/main/java/com/xyy/study/leetcode/dc/hash/Easy217.java
@@ -0,0 +1,21 @@
+package com.xyy.study.leetcode.dc.hash;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 给你一个整数数组 nums 。如果任一值在数组中出现 至少两次 ，返回 true ；
+ * 如果数组中每个元素互不相同，返回 false 。
+ * @author Xyy
+ * @create 2023-05-06 09:24
+ */
+public class Easy217 {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set =  new HashSet<>();
+        for (int num : nums) {
+            set.add(num);
+        }
+        // 注意是 !=,出现两次返回true,即有重复元素返回true
+        return set.size() != nums.length;
+    }
+}
---
+++ src/main/java/com/xyy/study/leetcode/dc/hash/Easy389.java
@@ -0,0 +1,35 @@
+package com.xyy.study.leetcode.dc.hash;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 给定两个字符串 s 和 t ，它们只包含小写字母。
+ * 字符串 t 由字符串 s 随机重排，然后在随机位置添加一个字母。
+ * 请找出在 t 中被添加的字母。
+ * @author Xyy
+ * @create 2023-05-06 09:32
+ */
+public class Easy389 {
+    public static void main(String[] args) {
+        System.out.println(0-'a');
+    }
+
+    public char findTheDifference(String s, String t) {
+
+        int[] target = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            target[ch-'a']++;
+        }
+        for (int i = 0; i < t.length(); i++) {
+            char ch = t.charAt(i);
+            target[ch-'a']--;
+            if (target[ch - 'a'] < 0) {
+                return ch;
+            }
+        }
+        return ' ';
+
+    }
+}
